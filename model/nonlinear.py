d88c432325103f7b2ffa3ea5a20882da52a2a961
krez <1307225752@qq.com>
2024-11-02 15:36:17 +0800

    change nc

--- model/nonlinear.py
+++ model/nonlinear.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 class NonLinear(nn.Module):
-    def __init__(self, in_nc=3, nc=100, out_nc=1):
+    def __init__(self, in_nc=3, nc=1600, out_nc=1):
         super(NonLinear, self).__init__()
 
         self.hidden = nn.Linear(in_nc, nc)
